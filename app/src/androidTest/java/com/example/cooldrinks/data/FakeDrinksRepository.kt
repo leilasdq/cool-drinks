c79aa97480a84c78c2c1780adeed7f46831a160d
leila_sdq <leila.sedighi.2020@gmail.com>
2022-12-27 23:40:07 +0330

    create class for repository test

---
+++ app/src/androidTest/java/com/example/cooldrinks/data/FakeDrinksRepository.kt
@@ -0,0 +1,44 @@
+package com.example.cooldrinks.data
+
+import com.example.cooldrinks.model.Drink
+import com.example.cooldrinks.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class FakeDrinksRepository(): DrinksRepository {
+    private var shouldShowError = false
+
+    fun changeShowErrorState(value: Boolean) {
+        shouldShowError = value
+    }
+
+    override fun getNonAlcoholicDrinks(): Flow<Resource<List<Drink>>> {
+        return if (shouldShowError) {
+            flowOf(
+                Resource.Error(Throwable("Error"))
+            )
+        } else {
+            flowOf(
+                Resource.Success(
+                    listOf()
+                )
+            )
+        }
+    }
+
+    override fun getDrinkDetail(id: Int): Flow<Resource<Drink>> {
+        return if (shouldShowError) {
+            flowOf(
+                Resource.Error(Throwable("Error"))
+            )
+        } else {
+            flowOf(
+                Resource.Success(
+                    Drink(
+                        "test name", "", "1", null, mapOf()
+                    )
+                )
+            )
+        }
+    }
+}
